103658c36048461e189b4c4bd58cae0e90f9c96d
Moriz von Langa <morizvlanga@gmail.com>
2025-01-17 15:05:46 +0100

    Added page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,8 @@
 import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import favicons from 'astro-favicons';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({applyBaseStyles: false}), favicons({
-        name: "Moriz von Langa",
-        short_name: "mvlanga",
-        background: "#000000"
-    })]
+    integrations: [react(), tailwind({applyBaseStyles: false})]
 });
