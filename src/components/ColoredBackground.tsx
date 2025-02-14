c3126e1199cbf76124357ab9216f973384987bda
Moriz von Langa <morizvlanga@gmail.com>
2025-02-14 11:11:46 +0100

    Adjusted about section

---
+++ src/components/ColoredBackground.tsx
@@ -0,0 +1,27 @@
+import { clsx } from "clsx";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+export const ColoredBackground = ({ className }: { className?: string }) => {
+	const ref = useRef(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+	});
+
+	return (
+		<motion.div
+			ref={ref}
+			className={twMerge(
+				clsx(
+					"-z-10 absolute top-0 right-0 bottom-0 left-0 bg-neutral-900",
+					className,
+				),
+			)}
+			style={{
+				opacity: useTransform(scrollYProgress, [0, 1], [0, 1]),
+			}}
+		/>
+	);
+};
