b0b3ab3b2c629b4e7a67e23672d94c193ca3e356
Moriz von Langa <morizvlanga@gmail.com>
2025-03-01 10:47:53 +0100

    adjusted fetch mastodon

--- src/utils/fetchMastodonStatuses.ts
+++ src/utils/fetchMastodonStatuses.ts
@@ -3,11 +3,19 @@ export const fetchMastodonStatuses = async <T>(userId: string) => {
 		`https://mastodon.social/api/v1/accounts/${userId}/statuses`,
 	);
 
+	console.log("RES BEFORE JSON");
+	console.log(response);
+
 	const data = await response.json();
+
+	console.log("JSON");
+	console.log(data);
 
 	if (response.ok) {
 		return data as unknown as T;
 	}
+
+	console.log("NOT OKKK");
 
 	throw new Error(
 		response.statusText ||
