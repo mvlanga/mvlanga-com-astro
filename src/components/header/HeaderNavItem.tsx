a8a92cf82d205c386191101b98e82c388275a8b0
Moriz von Langa <morizvlanga@gmail.com>
2025-03-22 14:14:09 +0100

    Added umami custom events

--- src/components/header/HeaderNavItem.tsx
+++ src/components/header/HeaderNavItem.tsx
@@ -1,72 +1,72 @@
-import {clsx} from "clsx";
-import {scroll} from "motion";
-import {inView} from "motion/react";
-import {type Dispatch, type SetStateAction, useEffect} from "react";
+import { clsx } from "clsx";
+import { scroll } from "motion";
+import { inView } from "motion/react";
+import { type Dispatch, type SetStateAction, useEffect } from "react";
 
 type HeaderNavItemProps = {
-    currentPath: string;
-    url: string;
-    label: string;
-    setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
-    onActiveAnchorLinkChange: Dispatch<SetStateAction<string[]>>;
-    isActive: boolean;
+	currentPath: string;
+	url: string;
+	label: string;
+	setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+	onActiveAnchorLinkChange: Dispatch<SetStateAction<string[]>>;
+	isActive: boolean;
 };
 
 type NavItemProps = Omit<HeaderNavItemProps, "currentPath">;
 
 export const HeaderNavItem = (props: HeaderNavItemProps) => {
-    if (props.url.includes("#")) {
-        return <AnchorNavItem {...props} />;
-    }
+	if (props.url.includes("#")) {
+		return <AnchorNavItem {...props} />;
+	}
 
-    return <NavItem {...props} />;
+	return <NavItem {...props} />;
 };
 
 const AnchorNavItem = (props: NavItemProps) => {
-    // biome-ignore lint/correctness/useExhaustiveDependencies: Should only run on mount
-    useEffect(() => {
-        scroll(() => {
-            inView(props.url.substring(1), () => {
-                const anchorEl = props.url.substring(1);
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Should only run on mount
+	useEffect(() => {
+		scroll(() => {
+			inView(props.url.substring(1), () => {
+				const anchorEl = props.url.substring(1);
 
-                props.onActiveAnchorLinkChange((current) => {
-                    if (current.includes(anchorEl)) {
-                        return current;
-                    }
+				props.onActiveAnchorLinkChange((current) => {
+					if (current.includes(anchorEl)) {
+						return current;
+					}
 
-                    return [...current, anchorEl];
-                });
+					return [...current, anchorEl];
+				});
 
-                return () => {
-                    props.onActiveAnchorLinkChange((current) => {
-                        return current.filter((a) => a !== anchorEl);
-                    });
-                };
-            });
-        });
-    }, []);
+				return () => {
+					props.onActiveAnchorLinkChange((current) => {
+						return current.filter((a) => a !== anchorEl);
+					});
+				};
+			});
+		});
+	}, []);
 
-    return <NavItem {...props} />;
+	return <NavItem {...props} />;
 };
 
-const NavItem = ({isActive, setIsMenuOpen, label, url}: NavItemProps) => {
-    return (
-        <a
-            href={url}
-            data-umami-event="link-click"
-            data-umami-event-url={url}
-            onClick={() => setIsMenuOpen(false)}
-            className="flex items-center"
-        >
-            <div
-                aria-hidden="true"
-                className={clsx(
-                    "-translate-x-14 absolute h-0.5 w-10 scale-x-0 rounded-xl bg-neutral-500 opacity-0 transition-all group-hover:scale-x-100 group-hover:opacity-100",
-                    isActive && "opacity-100",
-                    isActive && "scale-x-100",
-                )}
-            />
-            {label}
-        </a>
-    );
+const NavItem = ({ isActive, setIsMenuOpen, label, url }: NavItemProps) => {
+	return (
+		<a
+			href={url}
+			data-umami-event="link-click"
+			data-umami-event-url={url}
+			onClick={() => setIsMenuOpen(false)}
+			className="flex items-center"
+		>
+			<div
+				aria-hidden="true"
+				className={clsx(
+					"-translate-x-14 absolute h-0.5 w-10 scale-x-0 rounded-xl bg-neutral-500 opacity-0 transition-all group-hover:scale-x-100 group-hover:opacity-100",
+					isActive && "opacity-100",
+					isActive && "scale-x-100",
+				)}
+			/>
+			{label}
+		</a>
+	);
 };
