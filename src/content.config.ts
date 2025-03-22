5bf21c9f850aecbbcd163ac53f15c31615b5bcc7
Moriz von Langa <morizvlanga@gmail.com>
2025-03-22 17:30:55 +0100

    Added blog area

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,66 +1,66 @@
-import {defineCollection, z} from "astro:content";
-import {file, glob} from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders";
 
 const experience = defineCollection({
-    loader: glob({
-        base: "src/content/experience",
-        pattern: "*.json",
-    }),
-    schema: z.object({
-        company: z.object({
-            title: z.string(),
-            description: z.string(),
-        }),
-        from: z.number(),
-        to: z.string().optional(),
-        role: z.string(),
-        tasks: z.array(z.string()),
-        technologies: z.array(z.string()).optional(),
-    }),
+	loader: glob({
+		base: "src/content/experience",
+		pattern: "*.json",
+	}),
+	schema: z.object({
+		company: z.object({
+			title: z.string(),
+			description: z.string(),
+		}),
+		from: z.number(),
+		to: z.string().optional(),
+		role: z.string(),
+		tasks: z.array(z.string()),
+		technologies: z.array(z.string()).optional(),
+	}),
 });
 
 const projects = defineCollection({
-    loader: glob({
-        base: "src/content/projects",
-        pattern: "**/*.mdx",
-    }),
-    schema: ({image}) =>
-        z.object({
-            sortOrder: z.number(),
-            title: z.string(),
-            description: z.string(),
-            customer: z.string(),
-            cover: image(),
-            coverOnHover: image(),
-        }),
+	loader: glob({
+		base: "src/content/projects",
+		pattern: "**/*.mdx",
+	}),
+	schema: ({ image }) =>
+		z.object({
+			sortOrder: z.number(),
+			title: z.string(),
+			description: z.string(),
+			customer: z.string(),
+			cover: image(),
+			coverOnHover: image(),
+		}),
 });
 
 const legalPages = defineCollection({
-    loader: glob({
-        base: "src/content/legal",
-        pattern: "*.md",
-    }),
-    schema: z.object({
-        title: z.string(),
-    }),
+	loader: glob({
+		base: "src/content/legal",
+		pattern: "*.md",
+	}),
+	schema: z.object({
+		title: z.string(),
+	}),
 });
 
 const blogPosts = defineCollection({
-    loader: glob({
-        base: "src/content/blog",
-        pattern: "*.md",
-    }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        createdAt: z.date(),
-        tags: z.array(z.string()),
-    }),
+	loader: glob({
+		base: "src/content/blog",
+		pattern: "*.md",
+	}),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		createdAt: z.date(),
+		tags: z.array(z.string()),
+	}),
 });
 
 export const collections = {
-    experience,
-    projects,
-    legalPages,
-    blogPosts
+	experience,
+	projects,
+	legalPages,
+	blogPosts,
 };
