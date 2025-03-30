295b8ef11e187ea467509621e1fdfced6869e24d
Moriz von Langa <121946962+mvlanga@users.noreply.github.com>
2025-03-30 13:49:36 +0400

    refactor(component): added generic skeleton component

---
+++ src/components/common/Skeleton.tsx
@@ -0,0 +1,19 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+type SkeletonProps = {
+	className?: string;
+};
+
+export const Skeleton = ({ className }: SkeletonProps) => {
+	return (
+		<div
+			className={twMerge(
+				clsx(
+					"inline-block h-[1lh] w-full animate-pulse rounded-lg bg-neutral-800 transition-colors",
+					className,
+				),
+			)}
+		/>
+	);
+};
