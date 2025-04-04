5ca3eff25c0f0609b6f9e108836d283738421212
Moriz von Langa <morizvlanga@gmail.com>
2025-04-04 23:35:27 +0200

    refactor(blog): updated content of latest blog post

--- tests/blog-post-page.test.ts
+++ tests/blog-post-page.test.ts
@@ -3,8 +3,8 @@ import { testAutomaticallyDetectableA11nIssuesByRoute } from "./utils.ts";
 
 const blogPages: string[] = [
 	"/blog/organize-arrays-objects-dates-by-month-javascript-groupby",
-	"/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands"
-]
+	"/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
+];
 
 test.describe("blog-post-page", () => {
 	for (const route of blogPages) {
