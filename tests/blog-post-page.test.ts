ec8a020e6b176cd3931c2dd0d5f9aaa00a19600d
Moriz von Langa <morizvlanga@gmail.com>
2025-04-12 20:08:31 +0200

    chore(e2e): added latest blog post to e2e tests

--- tests/blog-post-page.test.ts
+++ tests/blog-post-page.test.ts
@@ -5,6 +5,7 @@ const blogPages: string[] = [
 	"/blog/organize-arrays-objects-dates-by-month-javascript-groupby",
 	"/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
 	"/blog/generating-open-graph-images-in-astro",
+	"/blog/forking-my-website",
 ];
 
 test.describe("blog-post-page", () => {
