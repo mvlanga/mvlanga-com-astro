e79ae8142c34b667cec76d5e679fd1b7a6ba4294
Moriz von Langa <morizvlanga@gmail.com>
2025-04-21 21:42:51 +0200

    feat(rss): adjusted rss details

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -30,8 +30,8 @@ export const GET: APIRoute = async (context) => {
 			({ id, data: { title, description, tags, createdAt } }) => ({
 				title,
 				description,
-				tags,
-				createdAt,
+				tags: tags.join(", "),
+				createdAt: createdAt.toString(),
 				link: `/blog/${id}/`,
 			}),
 		),
