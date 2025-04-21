bc2a86b447c5b151a4d51f09954d452ca34189c6
Moriz von Langa <morizvlanga@gmail.com>
2025-04-21 21:33:21 +0200

    feat(rss): added rss feed to blog posts

---
+++ src/pages/blog/rss.xml.ts
@@ -0,0 +1,36 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+
+const isProduction = import.meta.env.PROD;
+
+export const GET: APIRoute = async (context) => {
+	const blogPosts = await getCollection("blogPosts", ({ data }) => {
+		return isProduction
+			? data.draft === undefined || data.draft === false
+			: true;
+	});
+
+	if (context.site === undefined) {
+		return new Response(null, {
+			status: 500,
+			statusText: "GET context is missing property `site`",
+		});
+	}
+
+	return rss({
+		title: "Moriz’s Blog",
+		description:
+			"A few notes and reminders for myself, usually when I've found a solution to something I was struggling with, discovered a good solution to a channeling problem, or tried something new. It's just great to have a place to go back to.",
+		site: context.site,
+		items: blogPosts.map(
+			({ id, data: { title, description, tags, createdAt } }) => ({
+				title,
+				description,
+				tags,
+				createdAt,
+				link: `/blog/${id}/`,
+			}),
+		),
+	});
+};
