758945540ead5fff3ef62b21f2edcb0974d42297
Moriz von Langa <morizvlanga@gmail.com>
2025-06-01 21:02:35 +0200

    chore(ux): added rss icon to blog page

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,10 +1,12 @@
 import { getCollection } from "astro:content";
 import { sortBlogPosts } from "@/utils/sortCollection.ts";
 
-export const getBlogPosts = async (removeDrafts: boolean) =>
+const isProduction = import.meta.env.PROD;
+
+export const getBlogPosts = async () =>
 	sortBlogPosts(
 		await getCollection("blogPosts", ({ data }) => {
-			if (removeDrafts) {
+			if (isProduction) {
 				return data.draft === undefined || data.draft === false;
 			}
 
