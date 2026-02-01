387e299d1177d9b9be525911ba7643574c215096
Moriz von Langa <morizvlanga@gmail.com>
2026-02-01 13:30:51 +0100

    feature(considerDeploymentEnvironmentInPageViews): add hostname prefix to page view id if is deploy-preview

--- src/actions/pageViews.ts
+++ src/actions/pageViews.ts
@@ -2,33 +2,50 @@ import { ActionError, defineAction } from "astro:actions";
 import { db, inArray, PageViews, sql } from "astro:db";
 import { z } from "astro:schema";
 
+const enrichIdWithEnvironemtInfo = (id: string, hostname: string) => {
+	if (hostname.includes("deploy-preview")) {
+		return `${hostname}/${id}`;
+	}
+
+	return id;
+};
+
 export const pageViews = {
 	get: defineAction({
 		input: z.array(z.string()),
-		handler: async (ids) => {
+		handler: async (ids, context) => {
+			const idsWithEnvironmentInfo = ids.map((id) =>
+				enrichIdWithEnvironemtInfo(id, context.url.hostname),
+			);
+
 			try {
 				return await db
 					.select()
 					.from(PageViews)
-					.where(inArray(PageViews.id, ids));
+					.where(inArray(PageViews.id, idsWithEnvironmentInfo));
 			} catch (e) {
 				console.error(e);
 
 				throw new ActionError({
 					code: "BAD_REQUEST",
-					message: `Error getting "PageViews" with ids "${ids.join(", ")}"`,
+					message: `Error getting "PageViews" with ids "${idsWithEnvironmentInfo.join(", ")}"`,
 				});
 			}
 		},
 	}),
 	increase: defineAction({
 		input: z.string(),
-		handler: async (id) => {
+		handler: async (id, context) => {
+			const idWithEnvironmentInfo = enrichIdWithEnvironemtInfo(
+				id,
+				context.url.hostname,
+			);
+
 			try {
 				return await db
 					.insert(PageViews)
 					.values({
-						id,
+						id: idWithEnvironmentInfo,
 					})
 					.onConflictDoUpdate({
 						target: PageViews.id,
@@ -40,7 +57,7 @@ export const pageViews = {
 
 				throw new ActionError({
 					code: "BAD_REQUEST",
-					message: `Error increasing "PageViews" entry with id "${id}"`,
+					message: `Error increasing "PageViews" entry with id "${idWithEnvironmentInfo}"`,
 				});
 			}
 		},
