c8f934cae2f051ae631dcdf6f2ec8fc41c2a50f6
Moriz von Langa <morizvlanga@gmail.com>
2026-02-03 22:52:50 +0100

    fix(style): remove dark theme from experience

--- src/components/experience/ExperienceItem.tsx
+++ src/components/experience/ExperienceItem.tsx
@@ -4,15 +4,15 @@ export const ExperienceItem = ({
 	data: { from, to, role, company, tasks, technologies },
 }: CollectionEntry<"experience">) => {
 	return (
-		<div className="grid gap-6 rounded-xl bg-neutral-900 p-6 md:p-10 lg:grid-cols-3 lg:gap-10 light:bg-neutral-100">
+		<div className="grid gap-6 rounded-xl bg-neutral-100 p-6 md:p-10 lg:grid-cols-3 lg:gap-10">
 			<div className="flex flex-col gap-4 lg:col-span-1">
-				<p className="text-neutral-400 light:text-neutral-600">
+				<p className="text-neutral-600">
 					{from}
 					{" - "}
 					{to}
 				</p>
 				<h3 className="text-3xl">{role}</h3>
-				<p className="text-neutral-400 light:text-neutral-700">
+				<p className="text-neutral-700">
 					at <span className="font-semibold">{company.title}</span>,{" "}
 					{company.description}
 				</p>
@@ -21,14 +21,14 @@ export const ExperienceItem = ({
 			<hr className="border-neutral-800 lg:hidden" />
 
 			<div className="flex max-w-prose flex-col gap-8 lg:col-span-2">
-				<ul className="flex flex-col gap-4 text-neutral-400 light:text-neutral-700">
+				<ul className="flex flex-col gap-4 text-neutral-700">
 					{tasks.map((task) => (
 						<li>{task}</li>
 					))}
 				</ul>
 				<div className="flex flex-wrap gap-2">
 					{technologies.map((tech) => (
-						<span className="rounded-full bg-neutral-800 px-5 py-2 text-sm text-neutral-400 light:bg-purple-100 light:text-neutral-900">
+						<span className="rounded-full bg-neutral-200 px-5 py-2 text-sm text-neutral-900">
 							{tech}
 						</span>
 					))}
