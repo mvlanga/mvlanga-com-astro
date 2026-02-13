2e1b5d7c589b6d80f96cfabf14ed000cdbedcd31
Moriz von Langa <morizvlanga@gmail.com>
2026-02-13 17:09:08 +0100

    chore(deps): replace react with vue

--- astro.config.ts
+++ astro.config.ts
@@ -8,79 +8,76 @@ import { defineConfig } from "astro/config";
 import { visualizer } from "rollup-plugin-visualizer";
 import { EnumChangefreq } from "sitemap";
 
+import vue from "@astrojs/vue";
+
 export default defineConfig({
-	prefetch: {
-		prefetchAll: true,
-	},
-	markdown: {
-		shikiConfig: {
-			themes: {
-				light: "github-light-high-contrast",
-			},
-		},
-	},
-	site: "https://mvlanga.com",
-	integrations: [
-		sitemap({
-			serialize(item) {
-				if (item.url === "https://mvlanga.com/") {
-					item.changefreq = EnumChangefreq.MONTHLY;
-					item.lastmod = new Date().toDateString();
-					item.priority = 1;
-				}
+    prefetch: {
+        prefetchAll: true,
+    },
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: "github-light-high-contrast",
+            },
+        },
+    },
+    site: "https://mvlanga.com",
+    integrations: [sitemap({
+        serialize(item) {
+            if (item.url === "https://mvlanga.com/") {
+                item.changefreq = EnumChangefreq.MONTHLY;
+                item.lastmod = new Date().toDateString();
+                item.priority = 1;
+            }
 
-				if (/legal/.test(item.url)) {
-					item.changefreq = EnumChangefreq.YEARLY;
-					item.lastmod = new Date().toDateString();
-					item.priority = 0.2;
-				}
+            if (/legal/.test(item.url)) {
+                item.changefreq = EnumChangefreq.YEARLY;
+                item.lastmod = new Date().toDateString();
+                item.priority = 0.2;
+            }
 
-				if (/cariad|skoda|fujitsu/.test(item.url)) {
-					item.changefreq = EnumChangefreq.MONTHLY;
-					item.lastmod = new Date().toDateString();
-					item.priority = 0.9;
-				}
+            if (/cariad|skoda|fujitsu/.test(item.url)) {
+                item.changefreq = EnumChangefreq.MONTHLY;
+                item.lastmod = new Date().toDateString();
+                item.priority = 0.9;
+            }
 
-				if (/blog/.test(item.url)) {
-					item.changefreq = EnumChangefreq.MONTHLY;
-					item.lastmod = new Date().toDateString();
-					item.priority = 0.8;
-				}
+            if (/blog/.test(item.url)) {
+                item.changefreq = EnumChangefreq.MONTHLY;
+                item.lastmod = new Date().toDateString();
+                item.priority = 0.8;
+            }
 
-				return item;
-			},
-		}),
-		mdx(),
-		react(),
-		db(),
-	],
-	vite: {
-		plugins: [
-			tailwindcss(),
-			visualizer({
-				emitFile: true,
-				filename: "stats.html",
-			}),
-		],
-	},
-	adapter: netlify(),
-	redirects: {
-		"/:lang/personal-projects": "/",
-		"/:lang/about-me": "/#about",
-		"/assets/resume/*": "/moriz-von-langa_resume.pdf",
+            return item;
+        },
+		}), mdx(), react(), db(), vue()],
+    vite: {
+        plugins: [
+            tailwindcss(),
+            visualizer({
+                emitFile: true,
+                filename: "stats.html",
+            }),
+        ],
+    },
+    adapter: netlify(),
+    redirects: {
+        "/:lang/personal-projects": "/",
+        "/:lang/about-me": "/#about",
+        "/assets/resume/*": "/moriz-von-langa_resume.pdf",
 
-		"/:lang/skoda-enyaq-coupe": "/skoda-enyaq-coupe",
-		"/:lang/fujitsu-cx-lab": "/fujitsu-cx-lab",
-		"/:lang/cariad-iaa": "/cariad-iaa",
-		"/:lang/skoda-fabia": "/skoda-fabia",
-		"/:lang/fujitsu-smart-city": "/fujitsu-smart-city",
+        "/:lang/skoda-enyaq-coupe": "/skoda-enyaq-coupe",
+        "/:lang/fujitsu-cx-lab": "/fujitsu-cx-lab",
+        "/:lang/cariad-iaa": "/cariad-iaa",
+        "/:lang/skoda-fabia": "/skoda-fabia",
+        "/:lang/fujitsu-smart-city": "/fujitsu-smart-city",
 
-		"/:lang/legal/:slug": "/legal/:slug",
+        "/:lang/legal/:slug": "/legal/:slug",
 
-		"/en": "/",
-		"/de": "/",
+        "/en": "/",
+        "/de": "/",
 
-		"/blog/how-to-build-a-page-view-counter-with-astro-db-and-server-side-islands":
-			"/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
-	},
+        "/blog/how-to-build-a-page-view-counter-with-astro-db-and-server-side-islands":
+            "/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
+    },
 });
