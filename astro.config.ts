45abf0ef73fcb9443240383fbdb9553bb236fa80
Moriz von Langa <mvlanga@posteo.de>
2026-03-17 18:37:07 +0100

    Revert "use cloudflare adapter instead of netlify"
    This reverts commit 3773c819d7a0629d194a6d895b52224b2bae9fa8.

--- astro.config.ts
+++ astro.config.ts
@@ -1,80 +1,73 @@
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 import sitemap, { ChangeFreqEnum } from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import { visualizer } from "rollup-plugin-visualizer";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
-  prefetch: {
-      prefetchAll: true,
+	prefetch: {
+		prefetchAll: true,
 	},
-
-  markdown: {
-      shikiConfig: {
-          themes: {
-              light: "github-light-high-contrast",
-          },
-      },
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "github-light-high-contrast",
+			},
+		},
 	},
+	site: "https://mvlanga.com",
+	integrations: [
+		sitemap({
+			serialize(item) {
+				if (item.url === "https://mvlanga.com/") {
+					item.changefreq = ChangeFreqEnum.MONTHLY;
+					item.lastmod = new Date().toDateString();
+					item.priority = 1;
+				}
 
-  site: "https://mvlanga.com",
+				if (/legal/.test(item.url)) {
+					item.changefreq = ChangeFreqEnum.YEARLY;
+					item.lastmod = new Date().toDateString();
+					item.priority = 0.2;
+				}
 
-  integrations: [
-      sitemap({
-          serialize(item) {
-              if (item.url === "https://mvlanga.com/") {
-                  item.changefreq = ChangeFreqEnum.MONTHLY;
-                  item.lastmod = new Date().toDateString();
-                  item.priority = 1;
-              }
+				if (/blog/.test(item.url)) {
+					item.changefreq = ChangeFreqEnum.MONTHLY;
+					item.lastmod = new Date().toDateString();
+					item.priority = 0.8;
+				}
 
-              if (/legal/.test(item.url)) {
-                  item.changefreq = ChangeFreqEnum.YEARLY;
-                  item.lastmod = new Date().toDateString();
-                  item.priority = 0.2;
-              }
-
-              if (/blog/.test(item.url)) {
-                  item.changefreq = ChangeFreqEnum.MONTHLY;
-                  item.lastmod = new Date().toDateString();
-                  item.priority = 0.8;
-              }
-
-              return item;
-          },
-      }),
-      mdx(),
-      db(),
-      vue(),
+				return item;
+			},
+		}),
+		mdx(),
+		db(),
+		vue(),
 	],
-
-  vite: {
-      plugins: [
-          tailwindcss(),
-          visualizer({
-              emitFile: true,
-              filename: "stats.html",
-          }),
-      ],
+	vite: {
+		plugins: [
+			tailwindcss(),
+			visualizer({
+				emitFile: true,
+				filename: "stats.html",
+			}),
+		],
 	},
+	adapter: netlify(),
+	redirects: {
+		"/:lang/personal-projects": "/",
+		"/:lang/about-me": "/#about",
+		"/assets/resume/*": "/moriz-von-langa_resume.pdf",
 
-  redirects: {
-      "/:lang/personal-projects": "/",
-      "/:lang/about-me": "/#about",
-      "/assets/resume/*": "/moriz-von-langa_resume.pdf",
+		"/:lang/legal/:slug": "/legal/:slug",
 
-      "/:lang/legal/:slug": "/legal/:slug",
+		"/en": "/",
+		"/de": "/",
 
-      "/en": "/",
-      "/de": "/",
-
-      "/blog/how-to-build-a-page-view-counter-with-astro-db-and-server-side-islands":
-          "/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
+		"/blog/how-to-build-a-page-view-counter-with-astro-db-and-server-side-islands":
+			"/blog/how-to-build-a-page-view-counter-with-astro-db-actions-and-server-side-islands",
 	},
-
-  adapter: cloudflare(),
 });
