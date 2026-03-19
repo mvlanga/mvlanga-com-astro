95aacd30f37ac4c6c13d796aa3cd0f56202d8259
Moriz von Langa <morizvlanga@gmail.com>
2026-03-19 17:27:42 +0100

    restructure utils

--- src/utils/collections/utils.ts
+++ src/utils/collections/utils.ts
@@ -1,4 +1,17 @@
-import type { CollectionEntry } from "astro:content";
+import { type CollectionEntry, getCollection } from "astro:content";
+
+const isProduction = import.meta.env.PROD && !import.meta.env.IS_DEPLOY_PREVIEW;
+
+export const getBlogPosts = async () =>
+	sortBlogPosts(
+		await getCollection("blogPosts", ({ data }) => {
+			if (isProduction) {
+				return data.draft === undefined || data.draft === false;
+			}
+
+			return true;
+		}),
+	);
 
 export const sortExperience = <T extends CollectionEntry<"experience">>(
 	experience: T[],
