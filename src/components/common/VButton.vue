42d9753a4a968b2ffc9fdb560d5adf8bb765a7ad
Moriz von Langa <mvlanga@posteo.de>
2026-03-20 20:56:36 +0100

    fix build error

--- src/components/common/VButton.vue
+++ src/components/common/VButton.vue
@@ -46,7 +46,7 @@ const showActiveTextOnHover = computed(
 );
 
 const elementStyles = computed(() => ({
-	"group relative inline-flex items-center justify-center gap-2 overflow-clip cursor-pointer": true,
+	"group relative inline-flex cursor-pointer items-center justify-center gap-2 overflow-clip": true,
 	"rounded-2xl px-4 py-2": size.value === "small",
 	"rounded-3xl px-8 py-4": size.value === "medium",
 	"bg-purple-500 text-white": level.value === "primary",
