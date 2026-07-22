9fd75d7a4f98782c9b123f83fda733987f0ab1f5
Moriz von Langa <mvlanga@posteo.de>
2026-07-22 20:00:58 +0200

    adjust db import

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,10 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 const client = createClient({
-	url: import.meta.env.PROD
-		? import.meta.env.ASTRO_DB_REMOTE_URL
-		: "file:./dev.db",
-	authToken: import.meta.env.PROD
-		? import.meta.env.ASTRO_DB_APP_TOKEN
-		: undefined,
+	url: import.meta.env.ASTRO_DB_REMOTE_URL,
+	authToken: import.meta.env.ASTRO_DB_APP_TOKEN,
 });
 
 export const db = drizzle(client);
