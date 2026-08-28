d2792fe39567dbe8f1b31df30ab848a6c1b6624b
mvlanga <mvlanga@posteo.de>
2026-08-28 15:04:14 +0200

    adjuste tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
 	testDir: "./tests",
-	fullyParallel: false,
 	forbidOnly: !!process.env.CI,
 	timeout: 10_000,
 	retries: 0,
