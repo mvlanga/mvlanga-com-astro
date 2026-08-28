48e1c4b006035d9c11961f7dbda7ad8a46ab6a75
mvlanga <mvlanga@posteo.de>
2026-08-28 14:48:55 +0200

    adjuste tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
 	testDir: "./tests",
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
+	timeout: 10_000,
+	retries: 0,
 	reporter: "html",
 	use: {
 		baseURL: "http://localhost:4321",
